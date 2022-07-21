a94ec38eded764ba2bb46dddf3997e4b71461362
BCannon22 <BCannon14@my.gcu.edu>
2022-07-21 06:07:06 -0700

    Uploading activity 3

---
+++ Time Calculator/Time Calculator/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace Time_Calculator
+{
+    partial class TimeCalculator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.convertTime = new System.Windows.Forms.Button();
+            this.ClearText = new System.Windows.Forms.Button();
+            this.ExitButton = new System.Windows.Forms.Button();
+            this.Seconds = new System.Windows.Forms.Label();
+            this.secondsText = new System.Windows.Forms.TextBox();
+            this.ConvertedTime = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // convertTime
+            // 
+            this.convertTime.Location = new System.Drawing.Point(87, 107);
+            this.convertTime.Name = "convertTime";
+            this.convertTime.Size = new System.Drawing.Size(75, 23);
+            this.convertTime.TabIndex = 0;
+            this.convertTime.Text = "Time is:";
+            this.convertTime.UseVisualStyleBackColor = true;
+            this.convertTime.Click += new System.EventHandler(this.ConvertedTime_Click);
+            // 
+            // ClearText
+            // 
+            this.ClearText.Location = new System.Drawing.Point(59, 188);
+            this.ClearText.Name = "ClearText";
+            this.ClearText.Size = new System.Drawing.Size(75, 23);
+            this.ClearText.TabIndex = 1;
+            this.ClearText.Text = "Clear";
+            this.ClearText.UseVisualStyleBackColor = true;
+            this.ClearText.Click += new System.EventHandler(this.ClearText_Click);
+            // 
+            // ExitButton
+            // 
+            this.ExitButton.Location = new System.Drawing.Point(193, 188);
+            this.ExitButton.Name = "ExitButton";
+            this.ExitButton.Size = new System.Drawing.Size(75, 23);
+            this.ExitButton.TabIndex = 2;
+            this.ExitButton.Text = "Exit";
+            this.ExitButton.UseVisualStyleBackColor = true;
+            this.ExitButton.Click += new System.EventHandler(this.ExitButton_Click);
+            // 
+            // Seconds
+            // 
+            this.Seconds.AutoSize = true;
+            this.Seconds.Location = new System.Drawing.Point(56, 57);
+            this.Seconds.Name = "Seconds";
+            this.Seconds.Size = new System.Drawing.Size(106, 13);
+            this.Seconds.TabIndex = 3;
+            this.Seconds.Text = "How many seconds?";
+            // 
+            // secondsText
+            // 
+            this.secondsText.Location = new System.Drawing.Point(168, 54);
+            this.secondsText.Name = "secondsText";
+            this.secondsText.Size = new System.Drawing.Size(100, 20);
+            this.secondsText.TabIndex = 4;
+            // 
+            // ConvertedTime
+            // 
+            this.ConvertedTime.Location = new System.Drawing.Point(168, 107);
+            this.ConvertedTime.Name = "ConvertedTime";
+            this.ConvertedTime.ReadOnly = true;
+            this.ConvertedTime.Size = new System.Drawing.Size(100, 20);
+            this.ConvertedTime.TabIndex = 6;
+            // 
+            // TimeCalculator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(317, 288);
+            this.Controls.Add(this.ConvertedTime);
+            this.Controls.Add(this.secondsText);
+            this.Controls.Add(this.Seconds);
+            this.Controls.Add(this.ExitButton);
+            this.Controls.Add(this.ClearText);
+            this.Controls.Add(this.convertTime);
+            this.Name = "TimeCalculator";
+            this.Text = "Time Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button convertTime;
+        private System.Windows.Forms.Button ClearText;
+        private System.Windows.Forms.Button ExitButton;
+        private System.Windows.Forms.Label Seconds;
+        private System.Windows.Forms.TextBox secondsText;
+        private System.Windows.Forms.TextBox ConvertedTime;
+    }
+}
+
---
+++ Time Calculator/Time Calculator/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Time_Calculator
+{
+    public partial class TimeCalculator : Form
+    {
+        public TimeCalculator()
+        {
+            InitializeComponent();
+        }
+
+        private void ConvertedTime_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //Creating my variable for seconds.
+                double Seconds;
+                Seconds = double.Parse(secondsText.Text);
+
+                if (Seconds >= 60 && Seconds <= 3599)    // Sees if the number of seconds is in the range of minutes
+                {
+                    //Converts seconds into minutes
+                    double Minutes;       
+                    Minutes = Seconds / 60;
+
+                    //Sends the amount of mintues to the ConvertedTime textbox
+                    ConvertedTime.Text = Minutes.ToString("n2") + " minutes.";
+
+                }
+                else if (Seconds >= 3600 && Seconds <= 86399) //Sees if the seconds are with the range of hours
+                {
+                    //Converts seconds into hours
+                    double Hours;
+                    Hours = Seconds / 3600;
+
+                    //Sends the amount of hours to the ConvertedTIme textbox
+                    ConvertedTime.Text = Hours.ToString("n2") + " hours.";
+                }
+                else if (Seconds >= 86400) //Sees if the number of seconds is in the range of days
+                {
+                    //Converts seconds into days
+                    double Days;
+                    Days = Seconds / 86400;
+
+                    //Sends the amount of days to the converted number to the convertedTime textbox.
+                    ConvertedTime.Text = Days.ToString("n2") + " days.";
+                }
+                else if (Seconds >= 0 && Seconds <= 59) //Sees if the range is still within seconds
+                {
+                    //Shows the amount of seconds in ConvertedTime textbox
+                    ConvertedTime.Text = Seconds.ToString("n2") + " seconds";
+                }
+                else //catches negative number
+                {
+                    //Lets the user known negative numbers are not allowed.
+                    ConvertedTime.Text = "Incorrect Number";
+                }
+            }
+            catch (Exception ex)
+            {
+                //Displays error message
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void ClearText_Click(object sender, EventArgs e)
+        {
+            //Clears the text of the textboxes
+            secondsText.Text = "";
+            ConvertedTime.Text = "";
+        }
+
+        private void ExitButton_Click(object sender, EventArgs e)
+        {
+            //Closes the form
+            this.Close();
+        }
+    }
+}
