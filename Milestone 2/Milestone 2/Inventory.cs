222c5dbf851436bf64508076373811db2cd3cbc7
BCannon22 <BCannon14@my.gcu.edu>
2022-08-08 03:36:51 -0700

    MIlestone

---
+++ Milestone 2/Milestone 2/Inventory.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Milestone_2
+{
+    class Inventory
+    {
+         public static List<Product> ProductList { get; set; }
+
+        static Inventory()
+        {
+            ProductList = new List<Product>();
+        }
+        public void AddItem(Product product)
+        {
+            ProductList.Add(product);
+        }
+
+        public void RemoveItem(Product product)
+        {
+            ProductList.Remove(product);
+        }
+    }
+}
---
+++ Milestone2Form/Milestone2Form/Inventory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Milestone2Form
+{
+    internal class Inventory
+    {
+        List<Product> ProductList = new List<Product>();
+
+        public void AddItem(Product product)
+        {
+            ProductList.Add(product);
+        }
+    }
+}
