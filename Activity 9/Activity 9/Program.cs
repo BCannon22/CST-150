940f8fa91ccd5c62507fbe6092cd13c39aab9305
BCannon22 <BCannon14@my.gcu.edu>
2022-08-14 21:24:09 -0700

    Activity 9

---
+++ Activity 9/Activity 9/Program.cs
@@ -0,0 +1,52 @@
+﻿class StubMethods
+{
+    public int sum(int a, int b) 
+    {
+        return 0;
+    }  //Problem 1
+
+    public double average(double a, double b, double c, double d, double e)
+    {
+        return 0;
+    } //Problem 2
+
+    public int randSum(int randNumA, int randNumB)
+    {
+        return 0;
+    }   //Problem 3
+
+    public bool divisibleBy3(int a, int b, int c)
+    {
+        return false;
+    } //Problem 4
+
+    public bool fewerCharacters(string a, string b)
+    {
+        return false;
+    }  //Problem 5
+
+    public double arrayLargestNum(Array doubles, out double largestNum)
+    {
+        return largestNum = 0;
+    }  //Problem 6
+
+    public int arrayOfFiftyInts(int [] arr)
+    {
+        return 0;
+    }    //Problem 7
+    
+    public bool sameValue(bool a, bool b)
+    {
+        return true;
+    }   //Problem 8
+
+    public void intMultiplyByDouble(int a, double b)
+    {
+
+    } //Problem 9
+
+    public int twoDimArrayAverage(int[ , ] ints)  //Problem 10
+    {
+        return 0;
+    }
+}
