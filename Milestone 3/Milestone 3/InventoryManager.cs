d39e7159c185151f3519036c9e6f97d756d185a4
BCannon22 <BCannon14@my.gcu.edu>
2022-08-20 02:55:44 -0700

    Milestone 3, CST150

---
+++ Milestone 3/Milestone 3/InventoryManager.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Data;
+using System.ComponentModel;
+
+namespace Milestone_2
+{
+    class InventoryManager
+    {
+        public List<InventoryItem> ProductList { get; set; }   //Creates a list to hold products.
+
+        public InventoryManager()
+        {
+            ProductList = new List<InventoryItem>();
+        }
+        public void AddItem(InventoryItem product)    //Adds an item to inventory.
+        {
+            ProductList.Add(product);
+            Console.WriteLine($"Product {product.Name} with ID of #{product.Id} was added.");
+        }
+
+        public void RemoveItem(InventoryItem product)    //Removes an item from inventory.
+        {
+            ProductList.Remove(product);
+            Console.WriteLine($"Product {product.Name} with ID of #{product.Id} was removed.");
+        }
+
+        public void DisplayList()    //shows current inventory.
+        {
+            foreach (var product in ProductList)
+            {
+                Console.WriteLine($"ID #{product.Id}, Name: {product.Name}, Price:$ {product.Price}, Quantity: {product.Quantity}");
+            }
+        }
+
+        public void UpdateProduct(InventoryItem product)    //Updates an item.
+        {
+            product.Quantity = 0;
+            product.Price = 0;
+            Console.WriteLine($"Updated: ID #{product.Id}, Name: {product.Name}, Price:$ {product.Price}, Quantity: {product.Quantity}");
+        }
+
+        public void SearchProduct(InventoryItem product)    //Searches for an item based on id and name.
+        {           
+            foreach(InventoryItem item in ProductList)
+            {
+                if(item.Name.Equals(product.Name, StringComparison.CurrentCultureIgnoreCase) || item.Id.Equals(product.Id))
+                {
+                    Console.WriteLine($"Found ID#{product.Id}, Name: {product.Name}, Price:$ {product.Price}, Quantity: {product.Quantity}");
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ Milestone 3/Milestone 3/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Data;
+using System.ComponentModel;
+
+namespace Milestone_2
+{
+    class Test
+    {
+        static void Main(string[] args)
+        {
+            var donut = new InventoryItem(12, "Donut", 5.00m, 1);   //Creates products from the InventoryItem class
+            var tea = new InventoryItem(11, "Tea", 2.50m, 2);
+            var coffe = new InventoryItem(10, "Coffe", 2.00m, 4);
+            Console.WriteLine($"New product id #{donut.Id} was created for {donut.Name}, for a price of ${donut.Price}, and {donut.Quantity} was added.");
+            Console.WriteLine($"New product id #{tea.Id} was created for {tea.Name}, for a price of ${tea.Price}, and {tea.Quantity} was added.");
+            Console.WriteLine($"New product id #{coffe.Id} was created for {coffe.Name}, for a price of ${coffe.Price}, and {coffe.Quantity} was added.");
+            Console.ReadLine();
+
+
+            var inventory = new InventoryManager();   //calls for inventory manager class
+
+            inventory.AddItem(donut);  //adds item to inventory.
+            Console.WriteLine("Current Inventory is:");
+            inventory.DisplayList(); //shows current inventory.
+            Console.ReadLine();
+
+            inventory.AddItem(tea);
+            Console.WriteLine("Current Inventory is:");
+            inventory.DisplayList();
+            Console.ReadLine();
+            
+            inventory.AddItem(coffe);
+            Console.WriteLine("Current Inventory is:");
+            inventory.DisplayList();
+            Console.ReadLine();
+
+            inventory.RemoveItem(donut);  //Removes item from inventory.
+            Console.WriteLine("Current Inventory is:");
+            inventory.DisplayList(); //shows current inventory.
+            Console.ReadLine();
+
+            inventory.UpdateProduct(coffe); //Updates a product price/quantity.
+            Console.ReadLine();
+
+            inventory.SearchProduct(coffe);  //Searches inventory list by name and Id.
+            Console.ReadLine();
+
+            inventory.DisplayList();
+            Console.ReadLine();
+        }
+    }
+}
